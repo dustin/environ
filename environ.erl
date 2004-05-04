2a502e59e249c31e37aba4e81ac0be6c62d1d43d
Dustin Sallings <dustin@spy.net>
2004-05-04 16:36:35 +0000

    Split the supervisor and the application starting point.
    git-archimport-id: dustin@spy.net--projects-2004/environ--head--1.0--patch-11

--- environ.erl
+++ environ.erl
@@ -4,17 +4,15 @@
 
 -module(environ).
 
-% -behavior(supervisor).
 -behavior(application).
 
 % Supervisor functions.
--export([start_link/0, init/1, start/2, stop/1, config_change/3,
-	start_phase/3, prep_stop/1]).
+-export([start/2, stop/1, config_change/3, start_phase/3, prep_stop/1]).
 
 % application stuff
 start(_Type, _Args) ->
 	error_logger:info_msg("Starting environ~n", []),
-	start_link().
+	environ_sup:start_link().
 
 stop(_State) -> ok.
 
@@ -31,14 +29,3 @@ start_phase(Phase, StartType, PhaseArgs) ->
 prep_stop(State) ->
 	error_logger:info_msg("Prepping stop~n", []),
 	State.
-
-% Start up the environment server
-start_link() ->
-	supervisor:start_link(?MODULE, []).
-
-init(_Args) ->
-	{ok, {{one_for_one, 2, 60},
-		[{temp_listener, {temp_listener, start_link, []},
-			permanent, 5000, worker, [temp_listener]},
-		 {lemp_serv, {lemp_serv, start, []},
-		 	permanent, 5000, worker, [lemp_serv]}]}}.

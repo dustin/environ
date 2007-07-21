17ca0cbb20ec262140e7704250a0563cedd052a6
Dustin Sallings <dustin@spy.net>
2007-07-21 13:39:54 -0700

    A more useful setup_schema prog

--- src/setup_schema.erl
+++ src/setup_schema.erl
@@ -8,16 +8,22 @@
 -include("env_alert.hrl").
 
 db_nodes () ->
-	[temp@rubik, 'temp@dhcp-111'].
+	[temp@purple].
 
 init_schema() ->
-	mnesia:create_schema(db_nodes()).
+	mnesia:create_schema(db_nodes()),
+	ok = application:start(mnesia),
+	init_tables(),
+	ok = application:stop(mnesia).
 
 init_tables() ->
+	error_logger:info_msg("Creating alert_state", []),
 	{atomic, ok} = mnesia:create_table(alert_state,
 		[{disc_copies, db_nodes()},
 		{attributes, record_info(fields, alert_state)}]),
+	error_logger:info_msg("Creating therms", []),
 	{atomic, ok} = mnesia:create_table(therms,
 		[{disc_copies, db_nodes()},
 		{attributes, record_info(fields, therms)}]),
+	error_logger:info_msg("Done", []),
 	true.
